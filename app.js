b2479a6718efc47de1541cd0238525a6a7666328
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-14 13:40:24 +0200

    add styling

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.set("view engine", "ejs"); // sets EJS to work as the view engine
+app.use(bodyParser.urlencoded({ extended: true })); // sets bodyParser
+app.use(express.static("public")); // tells express to serve the public folder for styling or other static sites
 
-let item = "";
+let items = ["Buy food", "Cook food", "Eat food"];
+
 app.get("/", function (req, res) {
   let today = new Date();
   let options = {
@@ -21,12 +19,14 @@ app.get("/", function (req, res) {
   };
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { day: day, newListItem: item });
+  res.render("list", { day: day, newItems: items });
 });
 
 app.post("/", function (req, res) {
-  item = req.body.newItem;
-  res.redirect("/");
+  // this is fired by the form in the list.ejs file. it stores the value of the form's input in a variable called item.
+  let item = req.body.newItem;
+  items.push(item); // and then it pushes it into the items global array
+  res.redirect("/"); // refreshes the site, redirecting to the homepage (now showing up with the new, added item)
 });
 
 app.listen(3000, function () {
