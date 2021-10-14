8f05da907239030cd03f00ca5bef5ce20601ab01
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-14 14:06:38 +0200

    add Books path and functionality. use the same post to divert pushes depending on data templating info

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.set("view engine", "ejs"); // sets EJS to work as the view engine
 app.use(bodyParser.urlencoded({ extended: true })); // sets bodyParser
 app.use(express.static("public")); // tells express to serve the public folder for styling or other static sites
 
+//mock data
 let items = ["Buy food", "Cook food", "Eat food"];
+let bookItems = ["The Alchemist", "The four"];
 
+//Functionality for the home route (/)
 app.get("/", function (req, res) {
   let today = new Date();
   let options = {
@@ -19,16 +22,29 @@ app.get("/", function (req, res) {
   };
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { day: day, newItems: items });
+  res.render("list", { listTitle: day, newItems: items });
 });
 
 app.post("/", function (req, res) {
-  // this is fired by the form in the list.ejs file. it stores the value of the form's input in a variable called item.
-  let item = req.body.newItem;
-  items.push(item); // and then it pushes it into the items global array
-  res.redirect("/"); // refreshes the site, redirecting to the homepage (now showing up with the new, added item)
+  // this fires by the form in the list.ejs file. it stores the value of the form's input in a variable called item.
+  //now it checks whether the button from the form hast the Books Title or the date from the basic todo List. depending on which one, it will post the typed data in one list or the other.
+  if (req.body.button === "Books to read") {
+    let book = req.body.newItem;
+    bookItems.push(book);
+    res.redirect("/Books");
+  } else {
+    let item = req.body.newItem;
+    items.push(item); // and then it pushes it into the items global array
+    res.redirect("/"); // refreshes the site, redirecting to the homepage (now showing up with the new, added item)
+  }
 });
 
+//Functionality for the Books route
+app.get("/Books", function (req, res) {
+  res.render("list", { listTitle: "Books to read", newItems: bookItems });
+});
+
+//server
 app.listen(3000, function () {
   console.log("server started at port 3000");
 });
